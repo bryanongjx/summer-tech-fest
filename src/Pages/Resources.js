b41ae57032d5c29711292945cb1347f377c13a38
bryanongjxairetec <92636396+bryanongjx@users.noreply.github.com>
2023-05-16 23:26:36 +0800

    fix bug

--- src/Pages/Resources.js
+++ src/Pages/Resources.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ResourcesSidebar from '../Components/ResourcesSideBar';
-import './Resources.css'; // Import the custom CSS file
 
 const Resources = () => {
   return (
