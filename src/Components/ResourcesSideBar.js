0943a4d63a7c72059e25bd54ed6f3784ed5e8077
bryanongjxairetec <92636396+bryanongjx@users.noreply.github.com>
2023-05-21 15:04:28 +0800

    fix resources routing

--- src/Components/ResourcesSideBar.js
+++ src/Components/ResourcesSideBar.js
@@ -2,37 +2,42 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@coreui/coreui/dist/css/coreui.min.css';
 import { CSidebar, CSidebarNav, CNavItem, CNavGroup } from '@coreui/react';
+import { Link } from 'react-router-dom';
 
 const ResourcesSideBar = () => {
-  const basePath = '/summer-tech-fest'; // Replace with your desired base path
 
   return (
     <CSidebar>
       <CSidebarNav>
         <CNavGroup toggler="Getting Started">
-          <CNavItem href={`${basePath}/resources/techstack`}>Tech Stacks</CNavItem>
-          <CNavItem href={`${basePath}/resources/developmentlifecycles`}>Development Life Cycles</CNavItem>
-          <CNavItem href={`${basePath}/resources/choosingyouride`}>Choosing your IDE</CNavItem>
+          <Link to={`/resources/techstack`} className="nav-link">Tech Stacks</Link>
+          <Link to={`/resources/developmentlifecycles`} className="nav-link">Development Life Cycles</Link>
+          <Link to={`/resources/choosingyouride`} className="nav-link">Choosing your IDE</Link>
         </CNavGroup>
-        <CNavItem href={`${basePath}/resources/github`}>GitHub Version Control</CNavItem>
+
+        <CNavItem>
+            <Link to={`/resources/github`} className="nav-link">GitHub Version Control</Link>
+        </CNavItem>
 
         <CNavGroup toggler="App Development">
-          <CNavItem href={`${basePath}/resources/reactnative`}>React Native</CNavItem>
-          <CNavItem href={`${basePath}/resources/flutter`}>Flutter</CNavItem>
-          <CNavItem href={`${basePath}/resources/swift`}>Swift</CNavItem>
+          <Link to={`/resources/reactnative`} className="nav-link">React Native</Link>
+          <Link to={`/resources/flutter`} className="nav-link">Flutter</Link>
+          <Link to={`/resources/swift`} className="nav-link">Swift</Link>
         </CNavGroup>
 
         <CNavGroup toggler="Web Development">
-          <CNavItem href={`${basePath}/resources/reactjs`}>React.js</CNavItem>
-          <CNavItem href={`${basePath}/resources/angular`}>Angular</CNavItem>
+          <Link to={`/resources/reactjs`} className="nav-link">React.js</Link>
+          <Link to={`/resources/angular`} className="nav-link">Angular</Link>
         </CNavGroup>
 
         <CNavGroup toggler="Back End">
-          <CNavItem href={`${basePath}/resources/node`}>Node.js</CNavItem>
-          <CNavItem href={`${basePath}/resources/database`}>Database Storage</CNavItem>
+          <Link to={`/resources/node`} className="nav-link">Node.js</Link>
+          <Link to={`/resources/database`} className="nav-link">Database Storage</Link>
         </CNavGroup>
-
-        <CNavItem href={`${basePath}/resources/userguide`}>User Guide</CNavItem>
+        
+        <CNavItem>
+            <Link to={`/resources/userguide`} className="nav-link">User Guide</Link>
+        </CNavItem>
       </CSidebarNav>
     </CSidebar>
   );
